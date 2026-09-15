1707037c793d4e79c9d5eeda5f54384434b89e34
Aluno <pf95fabio@gmail.com>
2026-09-15 20:11:34 -0300

    feat: atualiazação do texto no index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View, StyleSheet } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
+      <Text style={styles.text}>TEXTO 100% ATUALIZADO</Text>
     </View>
   );
 }

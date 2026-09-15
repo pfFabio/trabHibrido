b51b2d8fd57e383c6fc60d7f918d234c2f1eb88a
Aluno <pf95fabio@gmail.com>
2026-09-15 20:13:18 -0300

    feat: atualiazação da cor de fundo no index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -11,7 +11,7 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#25292e",
+    backgroundColor: "#1fd19cff",
     alignItems: "center",
     justifyContent: "center",
   },
